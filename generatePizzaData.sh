e501b5710aa9963a794a9915f83ebd5074e71d05
runn3rman <grantgardner455@gmail.com>
2026-01-15 22:51:21 -0700

    Added database and other configs

---
+++ generatePizzaData.sh
@@ -0,0 +1,23 @@
+host="${1:-http://localhost:3000}"
+
+response=$(curl -s -X PUT "$host/api/auth" -d '{"email":"a@jwt.com", "password":"admin"}' -H 'Content-Type: application/json')
+token=$(echo "$response" | jq -r '.token')
+echo "$response"
+
+curl -X POST "$host/api/auth" -d '{"name":"pizza diner", "email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json'
+
+curl -X POST "$host/api/auth" -d '{"name":"pizza franchisee", "email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json'
+
+curl -X PUT "$host/api/order/menu" -H 'Content-Type: application/json' -d '{ "title":"Veggie", "description": "A garden of delight", "image":"pizza1.png", "price": 0.0038 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT "$host/api/order/menu" -H 'Content-Type: application/json' -d '{ "title":"Pepperoni", "description": "Spicy treat", "image":"pizza2.png", "price": 0.0042 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT "$host/api/order/menu" -H 'Content-Type: application/json' -d '{ "title":"Margarita", "description": "Essential classic", "image":"pizza3.png", "price": 0.0042 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT "$host/api/order/menu" -H 'Content-Type: application/json' -d '{ "title":"Crusty", "description": "A dry mouthed favorite", "image":"pizza4.png", "price": 0.0028 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT "$host/api/order/menu" -H 'Content-Type: application/json' -d '{ "title":"Charred Leopard", "description": "For those with a darker side", "image":"pizza5.png", "price": 0.0099 }'  -H "Authorization: Bearer $token"
+
+curl -X POST "$host/api/franchise" -H 'Content-Type: application/json' -d '{"name": "pizzaPocket", "admins": [{"email": "f@jwt.com"}]}'  -H "Authorization: Bearer $token"
+
+curl -X POST "$host/api/franchise/1/store" -H 'Content-Type: application/json' -d '{"franchiseId": 1, "name":"SLC"}'  -H "Authorization: Bearer $token"
